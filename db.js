4b98dcd7f8a9a1cd2cf6942c5ef0904a59ce1bae
efbingenieria <efbingenieria@gmail.com>
2024-07-08 14:36:50 -0300

    Modifico host de BD por una que funciona

--- db.js
+++ db.js
@@ -2,12 +2,13 @@ const mysql = require('mysql');
 
 // Configuración de la conexión a la base de datos MySQL
 const connection = mysql.createConnection({
-  host: process.env.HOST_DB,          // Actualizar host a la dirección del servidor
-  user: process.env.USER_DB,          // Usuario de la base de datos
-  password: process.env.PASSWORD_DB,  // Contraseña de la bse de datos        
-  database: process.env.DATABASE,     // Nombre de la base de datos
-  connectTimeout: 3000,               // Tiempo de espera de 3 segundos
-  //debug: true                         // Habilitar depuración
+  host: process.env.MYSQL_ADDON_HOST,           // Actualizar host a la dirección del servidor
+  user: process.env.MYSQL_ADDON_USER,           // Usuario de la base de datos
+  password: process.env.MYSQL_ADDON_PASSWORD,   // Contraseña de la bse de datos        
+  database: process.env.MYSQL_ADDON_DB,         // Nombre de la base de datos
+  port: process.env.MYSQL_ADDON_PORT,
+  connectTimeout: 5000,                         // Tiempo de espera de 3 segundos
+  //debug: true                                 // Habilitar depuración
 });
 
 // Conexión a la base de datos MySQL
